47e5f3e8f034c728ba5f54b2e4348dc479aa39f2
Cristina Urbina <cristinaurbinac20@gmail.com>
2023-05-06 19:41:15 -0600

    creacion del proyecto

---
+++ Proyecto1/include/TiposUsuario.h
@@ -0,0 +1,16 @@
+#ifndef TIPOSUSUARIO_H
+#define TIPOSUSUARIO_H
+
+
+class TiposUsuario
+{
+    public:
+        TiposUsuario() {}
+        virtual ~TiposUsuario() {}
+
+    protected:
+
+    private:
+};
+
+#endif // TIPOSUSUARIO_H
